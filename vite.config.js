45b9b0e4aa0723a215effc1bd51dc5c4904a03fe
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-03-02 02:04:43 +0100

    fix: ensure terser works, copy over static files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
-// import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [svelte(), tailwindcss()],
+	plugins: [
+		svelte(),
+		tailwindcss(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'static/*',
+					dest: 'static'
+				}
+			]
+		})
+	],
 	build: {
 		lib: {
 			entry: 'src/index.js',
@@ -13,13 +25,17 @@ export default defineConfig({
 			formats: ['es', 'umd', 'iife']
 		},
 		rollupOptions: {
-			// plugins: [
-			// 	terser({
-			// 		compress: {
-			// 			drop_console: false
-			// 		}
-			// 	})
-			// ],
+			plugins: [
+				terser({
+					keep_fnames: true,
+					keep_classnames: true,
+					ecma: 2020,
+					format: {
+						beautify: true,
+						comments: 'all'
+					}
+				})
+			],
 			output: {
 				// Provide global variables for use in UMD/IIFE build
 				globals: {
@@ -27,7 +43,7 @@ export default defineConfig({
 				}
 			}
 		},
-		minify: 'false', // can be terser
+		minify: 'terser',
 		sourcemap: true
 	}
 });
