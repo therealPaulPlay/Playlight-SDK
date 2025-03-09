bde04ea95025793c30def38fede86e8a6856c21c
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-03-09 18:57:31 +0100

    fix: don't trigger exit intent on page entry in certain scenarios

--- src/lib/components/ExitIntentDetector.svelte
+++ src/lib/components/ExitIntentDetector.svelte
@@ -1,44 +1,38 @@
 <script>
 	import { onMount } from "svelte";
+
 	let { enabled = true, onIntent } = $props();
 	let lastMouseY;
-	let isMouseInPage = false; // Flag to check if the mouse was previously outside the window
+	let lastMoveTimestamp = 0;
+	const moveThreshold = 100; // Time threshold in milliseconds
 
 	onMount(() => {
 		if (typeof window === "undefined") return;
-		lastMouseY = window.innerHeight; // Start as the bottom of the window
+		lastMouseY = window.innerHeight;
 	});
 </script>
 
 <svelte:document
-	onmouseleave={() => {
-		isMouseInPage = false;
-	}}
 	onmousemove={(event) => {
-		const currentMouseY = event.clientY;
-
-		// Check if the mouse is entering the page for the first time
-		if (!isMouseInPage) {
-			isMouseInPage = true;
-			lastMouseY = currentMouseY;
-			return;
-		}
-
-		lastMouseY = currentMouseY;
+		lastMouseY = event.clientY;
+		lastMoveTimestamp = Date.now();
 	}}
 />
 
 <!-- svelte-ignore a11y_mouse_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
-	class="fixed top-0 left-0 right-0 h-2 z-99990"
+	class="fixed top-0 right-0 left-0 z-99990 h-2"
 	onmouseover={(event) => {
 		const currentMouseY = event.clientY;
-		// Trigger popup only if mouse is moving upwards and was inside the page before
-		if (currentMouseY < lastMouseY && isMouseInPage) {
-			if (enabled && !localStorage.getItem("playlight_exit_intent_disabled_by_user")) onIntent?.();
+		const currentTime = Date.now();
+
+		// Check if the mouse on the page was moved shortly before (to prevent moving into the page from the browser chrome triggering it)
+		// And ensure the mouse is moving upwards and was inside the page before
+		if (currentMouseY < lastMouseY && currentTime - lastMoveTimestamp < moveThreshold) {
+			if (enabled && !localStorage.getItem("playlight_exit_intent_disabled_by_user")) {
+				onIntent?.();
+			}
 		}
-		// Update lastMouseY after checking
-		lastMouseY = currentMouseY;
 	}}
 ></div>
