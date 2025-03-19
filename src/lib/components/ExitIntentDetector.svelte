0725c84e986d9603a8f0f34d6ccd1c26d069cf06
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-03-19 16:55:54 +0100

    fix: detector bar too low down

--- src/lib/components/ExitIntentDetector.svelte
+++ src/lib/components/ExitIntentDetector.svelte
@@ -20,4 +20,4 @@
 
 <!-- svelte-ignore a11y_mouse_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div class="fixed top-2 right-0 left-0 h-4" onmouseover={handleBarTrigger}></div>
+<div class="fixed top-0 right-0 left-0 h-4" onmouseover={handleBarTrigger}></div>
